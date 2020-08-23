462b733d9dac561143d747c3ed53be7a0d4d5679
JorgeQ <alejandrojj2018@gmail.com>
2020-08-23 11:29:45 -0500

    agregando eventosa mi btn reservar en index con eventos show,shown,hide,hidden

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+console.log('Iniciado main')
 /*POPOVER*/
 $(function () {
 	$('[data-toggle="popover"]').popover()
@@ -8,3 +9,27 @@ $(function () {
 		container: 'body'
 	  })
 	})
+
+
+/*Modal - reservar*/
+
+$('#reservar-modal').on('show.bs.modal', function (e) {
+	console.log('Ejecutando modal con evento show')
+	$('#btn-reservar').removeClass('btn-outline-info')
+})
+
+$('#reservar-modal').on('shown.bs.modal', function (e) {
+	console.log('Ejecutando modal con evento shown')
+	$('#btn-reservar').addClass('btn-dark')
+	$('#btn-reservar').prop('disabled', true)
+})
+
+$('#reservar-modal').on('hide.bs.modal', function (e) {
+	console.log('Ejecutando modal con evento hide')
+	$('#btn-reservar').removeClass('btn-dark')
+})
+$('#reservar-modal').on('hidden.bs.modal', function (e) {
+	console.log('Ejecutando modal con evento hidden')
+	$('#btn-reservar').addClass('btn-outline-info')
+	$('#btn-reservar').prop('disabled', false)
+})
